3d06a82d2d8bc7b28e2f9c40db86bc3c515a4d10
Roberto Cestari <robertotcestari@gmail.com>
2022-04-21 16:52:04 -0300

    disable proptypes

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     "react/jsx-key": 1,
     "react-hooks/rules-of-hooks": 2,
     "react/prefer-stateless-function": 0,
+    "react/prop-types": 0,
     "react/jsx-filename-extension": [
       1,
       {
